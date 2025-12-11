a3c94cf949e63ea374f491d4a9ee3e6ac4d77763
Mykola-Ts <125122747+Mykola-Ts@users.noreply.github.com>
2025-12-11 10:39:30 +0000

    Deploying to gh-pages from @ Mykola-Ts/example-components@c8d704ee9b5813f4ca16b39be78e6822bebe88b6 🚀

---
+++ profile-card-page.7e8ee639.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll(".js-trigger"),c=document.querySelector(".js-profile-card");function t(){c.classList.toggle("active")}e.forEach((e=>e.addEventListener("click",t)));
+//# sourceMappingURL=profile-card-page.7e8ee639.js.map
---
+++ profile-card-page.95f6e9be.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelectorAll(".js-trigger"),t=document.querySelector(".js-profile-card");function c(){t.classList.toggle("active")}e.forEach((function(e){return e.addEventListener("click",c)}))}();
+//# sourceMappingURL=profile-card-page.95f6e9be.js.map
