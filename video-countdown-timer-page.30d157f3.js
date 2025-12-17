e632eb24fa452492b8719fd0d0cd739c8090372d
Mykola-Ts <125122747+Mykola-Ts@users.noreply.github.com>
2025-12-17 07:52:48 +0000

    Deploying to gh-pages from @ Mykola-Ts/example-components@867c9a2b94a3efe9ccf4fffb3dd0f4cc04e8e341 🚀

---
+++ video-countdown-timer-page.30d157f3.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".js-video-countdown-timer");document.addEventListener("visibilitychange",(()=>{document.hidden?e.pause():e.play()}));
+//# sourceMappingURL=video-countdown-timer-page.30d157f3.js.map
---
+++ video-countdown-timer-page.a046c417.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".js-video-countdown-timer");document.addEventListener("visibilitychange",(function(){document.hidden?e.pause():e.play()}))}();
+//# sourceMappingURL=video-countdown-timer-page.a046c417.js.map
