3c77b458abd8977248c5c3c1514350fdc6bcb510
Mykola-Ts <125122747+Mykola-Ts@users.noreply.github.com>
2026-01-22 10:58:02 +0000

    Deploying to gh-pages from @ Mykola-Ts/example-components@809f776956e152080cc61326f5691c61fe915422 🚀

--- components-page.106629bc.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".js-menu-btn");e.addEventListener("click",(function(){e.classList.toggle("open")}));
-//# sourceMappingURL=components-page.106629bc.js.map
--- components-page.3571bd5b.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".js-menu-btn");e.addEventListener("click",(function(){e.classList.toggle("open")}))}();
-//# sourceMappingURL=components-page.3571bd5b.js.map
